017de449e54fe87d88d3f4b945f9e665ff7987dc
Israel <45917510+Israel025@users.noreply.github.com>
2019-02-25 09:36:03 +0100

    solution for Feb. 22nd, daily challenge
    The program checks for a 2x2 Vowel matrix in an array of strings with equal length

---
+++ matrixString.js
@@ -0,0 +1,26 @@
+
+let strArr = ["aqrst", "uraei", "ffooo" ];
+
+function VowelSquare(stringArray){
+ 	let vowelRegEx = /a|e|i|o|u/gi;
+ 	let report = "2x2 matrix not found";
+
+	for (let arrayIndex = 0; arrayIndex < stringArray.length-1; arrayIndex++){
+  
+  let firstString = stringArray[arrayIndex];
+	let secondString = stringArray[arrayIndex+1];
+
+		for (let stringIndex = 0; stringIndex < firstString.length-1; stringIndex ++) {
+			(firstString[stringIndex].match(vowelRegEx)&&
+      firstString[stringIndex+1].match(vowelRegEx)&&
+      secondString[stringIndex].match(vowelRegEx)&&
+      secondString[stringIndex+1].match(vowelRegEx))
+      ?report =`${arrayIndex} - ${stringIndex-1}`:false;
+	} 
+
+} 
+
+	 return report;
+}
+
+console.log(VowelSquare(strArr) );
